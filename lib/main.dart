cb37b560164f9df3b74e186dc3765801c0c72d0c
JoJoPuppe <marcusloeper@gmx.de>
2024-01-09 23:54:26 +0100

    refactor home view

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:home_inventory_app/views/items/view_item.dart';
-import 'views/settings/settings_screen.dart';
 import 'views/items/add_item.dart';
 import 'provider/settings_provider.dart';
 import 'package:provider/provider.dart';
-import '/models/item_model.dart';
-import '/services/homeinventory_api_service.dart'; // Import the file where you define the API call
 import '/provider/camera_manager.dart';
-import '/views/items/list_item.dart';
+import '/views/home/home.dart';
 import 'views/items/search_view.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
@@ -59,77 +55,26 @@ class InventoryHomePage extends StatefulWidget {
 class _InventoryHomePageState extends State<InventoryHomePage> {
   final PersistentTabController _controller =
         PersistentTabController(initialIndex: 0);
-  List<Item> _items = [];
-  String apiDomain = '';
-
-
-  Future<List<Item>> getAllItems() async {
-    List<Item> insideItems = await CreateItemService.getChildren(context, null);
-    return insideItems;
-  }
-
-  String buildImageUrl(String image) {
-    String apiDomain = Provider.of<SettingsProvider>(context, listen: false).currentSettings.serverURL;
-    return "$apiDomain/$image";
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchInitialData();
-    apiDomain = Provider.of<SettingsProvider>(context, listen: false).currentSettings.serverURL;
-  }
-
-  Future<void> _fetchInitialData() async {
-    final newItems = await getAllItems();
-    setState(() {
-       _items = newItems;
-    });
-  }
-  void _onItemTap(Item item) {
-    PersistentNavBarNavigator.pushNewScreen(
-      context,
-      screen: ViewItem(item: item),
-      withNavBar: true,
-      pageTransitionAnimation: PageTransitionAnimation.fade,
-    );
-  }
-  //ignore: must_call_super
-  // Future<List<Item>>? _itemList;
+  BuildContext? _tabContext;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            tooltip: 'Settings',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const SettingsScreen(),
-                )
-              );
-            }
-          )
-        ]
-      ),
-      body: PersistentTabView(
+    return PersistentTabView(
+        selectedTabScreenContext: (BuildContext? tabContext) => _tabContext = tabContext,
         context,
-        hideNavigationBar: false,
+        backgroundColor: Colors.transparent,
         controller: _controller,
+        popAllScreensOnTapAnyTabs: true,
         screens: [
-          newItemListWidget(),
+          HomeView(tabContext: _tabContext),
           const AddItem(),
           const SearchView(),
         ],
         items: _navBarItems(),
-        navBarStyle: NavBarStyle.style3,
-        )
-
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        navBarStyle: NavBarStyle.style15,
     );
   }
   List<PersistentBottomNavBarItem> _navBarItems() {
@@ -141,8 +86,7 @@ class _InventoryHomePageState extends State<InventoryHomePage> {
         inactiveColorPrimary: Colors.grey,
       ),
       PersistentBottomNavBarItem(
-        icon: const Icon(Icons.add),
-        title: ("Add"),
+        icon: const Icon(Icons.add, color: Colors.white),
         activeColorPrimary: Colors.blue,
         inactiveColorPrimary: Colors.grey,
       ),
@@ -153,42 +97,5 @@ class _InventoryHomePageState extends State<InventoryHomePage> {
         inactiveColorPrimary: Colors.grey,
       ),
     ];
-  }
-
-  Widget newItemListWidget() {
-    return RefreshIndicator(
-      onRefresh: () async {
-        final newItems = await getAllItems();
-        setState(() {
-          _items = newItems;
-        });
-      },
-      child: _items.isNotEmpty
-      ? ListView.builder(
-          itemCount: _items.length,
-          itemBuilder: (context, index) {
-            return ListItem(item: _items[index], onTap: _onItemTap, apiDomain: apiDomain, context: context);
-          },
-        )
-      : LayoutBuilder(
-          builder: (context, constraints) {
-            return SingleChildScrollView(
-              physics: const AlwaysScrollableScrollPhysics(),
-              child: SizedBox(
-                height: constraints.maxHeight,
-                child: const Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('There is not data.'),
-                      Text('Pull to refresh.'),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-        )
-    );
   }
 }
---
+++ lib/views/home/home.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import '/views/items/list_item.dart';
+import '/services/homeinventory_api_service.dart'; // Import the file where you define the API call
+import '/views/items/view_item.dart';
+import '/models/item_model.dart';
+import '/provider/settings_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import '/views/settings/settings_screen.dart';
+
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key, required this.tabContext});
+  final BuildContext? tabContext;
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  List<Item> _items = [];
+  String apiDomain = '';
+
+  Future<List<Item>> getAllItems() async {
+    List<Item> insideItems = await CreateItemService.getChildren(context, null);
+    return insideItems;
+  }
+
+  String buildImageUrl(String image) {
+    String apiDomain = Provider.of<SettingsProvider>(context, listen: false).currentSettings.serverURL;
+    return "$apiDomain/$image";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchInitialData();
+    apiDomain = Provider.of<SettingsProvider>(context, listen: false).currentSettings.serverURL;
+  }
+
+  Future<void> _fetchInitialData() async {
+    final newItems = await getAllItems();
+    setState(() {
+       _items = newItems;
+    });
+  }
+  void _onItemTap(Item item) {
+    PersistentNavBarNavigator.pushNewScreen(
+      widget.tabContext ?? context,
+      screen: ViewItem(item: item),
+      withNavBar: true,
+      pageTransitionAnimation: PageTransitionAnimation.fade,
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Inventory"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Settings',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsScreen(),
+                )
+              );
+            }
+          )
+        ]
+      ),
+      body: newItemListWidget(),
+    );
+  }
+  Widget newItemListWidget() {
+    return RefreshIndicator(
+      onRefresh: () async {
+        final newItems = await getAllItems();
+        setState(() {
+          _items = newItems;
+        });
+      },
+      child: _items.isNotEmpty
+      ? ListView.builder(
+          itemCount: _items.length,
+          itemBuilder: (context, index) {
+            return ListItem(item: _items[index], onTap: _onItemTap, apiDomain: apiDomain, context: context);
+          },
+        )
+      : LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: SizedBox(
+                height: constraints.maxHeight,
+                child: const Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('There is not data.'),
+                      Text('Pull to refresh.'),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        )
+    );
+  }
+}
+
